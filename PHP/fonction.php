405d4ed32774acba7ff675bfc5a1ac37e5c6fdda
GHCrystal <pa.bonnel@laposte.net>
2013-11-04 17:26:37 +0100

    Update fonction.php

--- PHP/fonction.php
+++ PHP/fonction.php
@@ -16,11 +16,13 @@ function affichage_fin_page() {
   </html>");
   }
 // --------- FONCTION DE CONTROLE ----------
+
 //Fonction PHP pour controler une adresse Mail
 function validateMail($email)
 {
 	return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
+
 //fonction pour vérifier la conformité d'un numéro de tel Français
 function VerifTelFr($chaine)
 {
@@ -29,5 +31,15 @@ function VerifTelFr($chaine)
 	     return true;
 	  else
 	     return false;
+}
+
+# Vérifie la validité d'une date
+function is_valide_date($date, $sep='/')
+{
+	if(!list($day, $month, $year) = explode($sep, $date))
+		return false;
+	if($day > 31 OR $day < 1 OR $month > 12 OR $month < 1 OR $year > 32767 OR $year < 1)
+		return false;
+	return checkdate($month, $day, $year);
 }
 ?>
