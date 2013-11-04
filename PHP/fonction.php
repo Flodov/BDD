d184a8af7ed73cb0c27e8723b59a8658c27c506f
GHCrystal <pa.bonnel@laposte.net>
2013-11-04 17:13:19 +0100

    Update fonction.php

--- PHP/fonction.php
+++ PHP/fonction.php
@@ -15,5 +15,10 @@ function affichage_fin_page() {
   print("</body>
   </html>");
   }
-
+// --------- FONCTION DE CONTROLE ----------
+//Fonction PHP pour controler une adresse Mail
+function validateMail($email)
+{
+	return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
 ?>
