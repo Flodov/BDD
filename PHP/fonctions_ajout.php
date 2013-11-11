08429201ad4180f567be75e680880ade7ed8d2d1
GHCrystal <pa.bonnel@laposte.net>
2013-11-11 22:10:51 +0100

    Update fonctions_ajout.php

--- PHP/fonctions_ajout.php
+++ PHP/fonctions_ajout.php
@@ -2,7 +2,7 @@
 
 function ajouter_personne($nom, $prenom, $email, $accompagnateur )
 {
-	$link = mysqli_connect("localhost",'root','web', "projet_bdd_iut");
+  $link = mysqli_connect("localhost",'root','web', "projet_bdd_iut");
   if($link==NULL)die("pb connexion");
   $query = "INSERT INTO PERSONNE (nom_personne, prenom, adresse_email, accompagnateur) VALUES ('$nom',$prenom,'$email',$accompagnateur);";
   if (!mysqli_query($link, $query)) 
