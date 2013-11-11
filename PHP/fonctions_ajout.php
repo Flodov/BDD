a2d2e600d8fbb0dc3bd6ac51afc94b29765a628d
GHCrystal <pa.bonnel@laposte.net>
2013-11-11 22:34:29 +0100

    Update fonctions_ajout.php

--- PHP/fonctions_ajout.php
+++ PHP/fonctions_ajout.php
@@ -18,7 +18,37 @@ function ajouter_type_manifestation($libelle)
 {
 	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
 	if($link==NULL)die("pb connexion");
-	$requete="INSERT INTO TYPE_EMPLACEMENT values ('$libelle');";
+	$requete="INSERT INTO TYPE_EMPLACEMENT (libelle_type_emplacement) values ('$libelle');";
+  	if (!mysqli_query($link, $requete)) 
+    	print("Message d'erreur :".mysqli_error($link)); 
+  	mysqli_close($link);
+}
+
+function ajouter_tel($tel, $num_personne)
+{
+	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
+	if($link==NULL)die("pb connexion");
+	$requete="INSERT INTO TYPE_EMPLACEMENT (numTelephone, id_personne) values ($tel, $num_personne);";
+  	if (!mysqli_query($link, $requete)) 
+    	print("Message d'erreur :".mysqli_error($link)); 
+  	mysqli_close($link);
+}
+
+function ajouter_lieu($libelle)
+{
+	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
+	if($link==NULL)die("pb connexion");
+	$requete="INSERT INTO LIEU (nom_lieu) values ('$libelle');";
+  	if (!mysqli_query($link, $requete)) 
+    	print("Message d'erreur :".mysqli_error($link)); 
+  	mysqli_close($link);
+}
+
+function ajouter_participe($id_personne, $id_activite, $date_participe)
+{
+	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
+	if($link==NULL)die("pb connexion");
+	$requete="INSERT INTO PARTICIPE (id_personne, id_activite, date_participe) values ($id_personne, $id_activite, '$date_participe' );";
   	if (!mysqli_query($link, $requete)) 
     	print("Message d'erreur :".mysqli_error($link)); 
   	mysqli_close($link);
