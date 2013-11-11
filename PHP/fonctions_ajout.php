ea744e060f2bc381a92d478db10abcee3ccca0df
GHCrystal <pa.bonnel@laposte.net>
2013-11-11 22:41:09 +0100

    Update fonctions_ajout.php

--- PHP/fonctions_ajout.php
+++ PHP/fonctions_ajout.php
@@ -28,7 +28,7 @@ function ajouter_tel($tel, $num_personne)
 {
 	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
 	if($link==NULL)die("pb connexion");
-	$requete="INSERT INTO TYPE_EMPLACEMENT (numTelephone, id_personne) values ($tel, $num_personne);";
+	$requete="INSERT INTO TEL (numTelephone, id_personne) values ($tel, $num_personne);";
   	if (!mysqli_query($link, $requete)) 
     	print("Message d'erreur :".mysqli_error($link)); 
   	mysqli_close($link);
@@ -49,6 +49,26 @@ function ajouter_participe($id_personne, $id_activite, $date_participe)
 	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
 	if($link==NULL)die("pb connexion");
 	$requete="INSERT INTO PARTICIPE (id_personne, id_activite, date_participe) values ($id_personne, $id_activite, '$date_participe' );";
+  	if (!mysqli_query($link, $requete)) 
+    	print("Message d'erreur :".mysqli_error($link)); 
+  	mysqli_close($link);
+}
+
+function ajouter_anime($id_personne, $id_activite, $date_anime)
+{
+	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
+	if($link==NULL)die("pb connexion");
+	$requete="INSERT INTO ANIME (id_personne, id_activite, date_anime) values ($id_personne, $id_activite, '$date_anime' );";
+  	if (!mysqli_query($link, $requete)) 
+    	print("Message d'erreur :".mysqli_error($link)); 
+  	mysqli_close($link);
+}
+
+function ajouter_RESERVATION($montant_total, $date_debut, $date_fin, $id_personne, $id_emplacement)
+{
+	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
+	if($link==NULL)die("pb connexion");
+	$requete="INSERT INTO RESERVATION (montant_total, date_debut, date_fin, id_personne, id_emplacement) values ($montant_total, '$date_debut', '$date_fin', $id_personne, id_emplacement );";
   	if (!mysqli_query($link, $requete)) 
     	print("Message d'erreur :".mysqli_error($link)); 
   	mysqli_close($link);
