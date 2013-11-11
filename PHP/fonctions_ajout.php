9afa6065d721a0ee4b0bb16d2f9f8ed51e5cc57e
GHCrystal <pa.bonnel@laposte.net>
2013-11-11 22:18:54 +0100

    Update fonctions_ajout.php

--- PHP/fonctions_ajout.php
+++ PHP/fonctions_ajout.php
@@ -6,20 +6,20 @@ $bdd="projet_bdd_iut";
 
 function ajouter_personne($nom, $prenom, $email, $accompagnateur )
 {
-  $link = mysqli_connect("localhost",'$user','$web', "$bdd");
-  if($link==NULL)die("pb connexion");
-  $query = "INSERT INTO PERSONNE (nom_personne, prenom, adresse_email, accompagnateur) VALUES ('$nom',$prenom,'$email',$accompagnateur);";
-  if (!mysqli_query($link, $query)) 
-    print("Message d'erreur :".mysqli_error($link)); 
-  mysqli_close($link);
+	$link = mysqli_connect("localhost",'$user','$web', "$bdd");
+  	if($link==NULL)die("pb connexion");
+  	$query = "INSERT INTO PERSONNE (nom_personne, prenom, adresse_email, accompagnateur) VALUES ('$nom',$prenom,'$email',$accompagnateur);";
+  	if (!mysqli_query($link, $query)) 
+    	print("Message d'erreur :".mysqli_error($link)); 
+  	mysqli_close($link);
 }
 
 function ajouter_type_manifestation($libelle)
 {
 	$link = mysqli_connect("localhost",'$user','$web', "$bdd";
-  if($link==NULL)die("pb connexion");
-  $requete="INSERT INTO TYPE_EMPLACEMENT values ('$libelle');";
-  if (!mysqli_query($link, $requete)) 
-    print("Message d'erreur :".mysqli_error($link)); 
-  mysqli_close($link);
+	if($link==NULL)die("pb connexion");
+	$requete="INSERT INTO TYPE_EMPLACEMENT values ('$libelle');";
+  	if (!mysqli_query($link, $requete)) 
+    	print("Message d'erreur :".mysqli_error($link)); 
+  	mysqli_close($link);
 }
