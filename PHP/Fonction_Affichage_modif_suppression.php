da708d0135a0d1b7adb7b2552d5cfca4c39546da
Serobi <paul.nelaton@gmail.com>
2013-11-13 20:47:03 +0100

    Update Fonction_Affichage_modif_suppression.php

--- PHP/Fonction_Affichage_modif_suppression.php
+++ PHP/Fonction_Affichage_modif_suppression.php
@@ -6,7 +6,7 @@ $mdp = web;
 $bdd = projet_bdd_iut;
 
 
-function affichage_activite() {
+function affichage_Activite() {
   $connexion = mysqli_connect($host,$user,$mdp,$bdd);
   if($connexion==NULL) {
     die("pb connexion");
@@ -48,8 +48,69 @@ function affichage_Lieu() {
   print("</table>");
 }
 
+function affichage_Tel() {
+  $connexion = mysqli_connect($host,$user,$mdp,$bdd);
+  if($connexion==NULL) {
+    die("pb connexion");
+    return;
+  }
+  $req = "SELECT t.numTelephone p.nom_personne FROM Tel t, Personne p WHERE t.id_personne = p.id_personne";
+  $result = $connexion -> $req;
+  print("<table>
+         <tr><th colspan=\"2\">Telephone</th></tr>
+         <tr><th>Numero de telephone</th><th>Nom de la personne</th><th>Actions</th></tr>");
+  while($ligne = $result -> Fetch_array()) {
+    print("<tr><td>".$ligne[0]."/<td><td>".$ligne[1]."/<td></tr>
+    <td style=\"text-align : center;\"><a href=\"affichage.php?modif=".$ligne[0]."\">
+    <img src=\"modif.PNG\" alt=\"modifier\" style=\"width : 20px; height : 20px;\"/></a>
+    <a href=\"affichage.php?suppr=".$ligne[0]."\"><img src=\"suppr.PNG\" alt=\"faux\" 
+    style=\" width : 20px; height=20px;\"/></a></td></tr>"); 
+  }
+  print("</table>");
+}
 
+function affichage_Emplacement() {
+  $connexion = mysqli_connect($host,$user,$mdp,$bdd);
+  if($connexion==NULL) {
+    die("pb connexion");
+    return;
+  }
+  $req = "SELECT e.id_emplacement, e.prix_emplacement, te.libelle_type_emplacement FROM Emplacement e,
+  Type_Emplacement te WHERE e.id_type_emplacement = te.id_type_emplacement";
+  $result = $connexion -> $req;
+  print("<table>
+         <tr><th colspan=\"3\">Emplacement</th></tr>
+         <tr><th>Prix emplacement</th><th>Libelle type emplacement</th><th>Actions</th></tr>");
+  while($ligne = $result -> Fetch_array()) {
+    print("<tr><td style=\"display:none;\">".$ligne[0]."</td><td>".$ligne[1]."/<td><td>".$ligne[2]."/<td></tr>
+    <td style=\"text-align : center;\"><a href=\"affichage.php?modif=".$ligne[0]."\">
+    <img src=\"modif.PNG\" alt=\"modifier\" style=\"width : 20px; height : 20px;\"/></a>
+    <a href=\"affichage.php?suppr=".$ligne[0]."\"><img src=\"suppr.PNG\" alt=\"faux\" 
+    style=\" width : 20px; height=20px;\"/></a></td></tr>"); 
+  }
+  print("</table>");
+}
 
+function affichage_Type_Emplacement() {
+  $connexion = mysqli_connect($host,$user,$mdp,$bdd);
+  if($connexion==NULL) {
+    die("pb connexion");
+    return;
+  }
+  $req = "SELECT * FROM Type_Emplacement";
+  $result = $connexion -> $req;
+  print("<table>
+         <tr><th colspan=\"1\">Type Emplacement</th></tr>
+         <tr><th>Libelle type emplacement</th><th>Actions</th></tr>");
+  while($ligne = $result -> Fetch_array()) {
+    print("<tr><td style=\"display:none;\">".$ligne[0]."</td><td>".$ligne[1]."/<td></tr>
+    <td style=\"text-align : center;\"><a href=\"affichage.php?modif=".$ligne[0]."\">
+    <img src=\"modif.PNG\" alt=\"modifier\" style=\"width : 20px; height : 20px;\"/></a>
+    <a href=\"affichage.php?suppr=".$ligne[0]."\"><img src=\"suppr.PNG\" alt=\"faux\" 
+    style=\" width : 20px; height=20px;\"/></a></td></tr>"); 
+  }
+  print("</table>");
+}
 
 
 ?>
