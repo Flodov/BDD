ff619bc92be156df988a85bfe7aa846b4ca5ed22
Serobi <paul.nelaton@gmail.com>
2013-11-17 23:47:37 +0100

    Update Fonction_Affichage_modif_suppression.php

--- PHP/Fonction_Affichage_modif_suppression.php
+++ PHP/Fonction_Affichage_modif_suppression.php
@@ -206,5 +206,28 @@ function affichage_facture() {
   print("</table>");
 }
 
+// question date, je le finis demain.
+function affichage_anime() {
+  $connexion = mysqli_connect($host,$user,$mdp,$bdd);
+  if($connexion==NULL) {
+    die("pb connexion");
+    return;
+  }
+  $req = "SELECT a.id_activite, a.nom_activite, l.nom_lieu 
+  FROM Anime a, activite ac
+  WHERE a.id_lieu = l.id_lieu";
+  $result = $connexion -> $req;
+  print("<table>
+         <tr><th colspan=\"3\">Anime</th></tr>
+         <tr><th>Nom Activité</th><th>Lieu</th><th>Actions</th></tr>");
+  while($ligne = $result -> Fetch_array()) {
+    print("<tr><td style=\"display:none;\">".$ligne[0]."</td><td>".$ligne[1]."/<td><td>".$ligne[2]."</td>
+    <td style=\"text-align : center;\"><a href=\"affichage.php?modif=".$ligne[0]."\">
+    <img src=\"modif.PNG\" alt=\"modifier\" style=\"width : 20px; height : 20px;\"/></a>
+    <a href=\"affichage.php?suppr=".$ligne[0]."\"><img src=\"suppr.PNG\" alt=\"faux\" 
+    style=\" width : 20px; height=20px;\"/></a></td></tr>"); 
+  }
+  print("</table>");
+}
 
 ?>
