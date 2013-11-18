755d9062f229d9867d62c680105d940c1ab70ba3
GHCrystal <pa.bonnel@laposte.net>
2013-11-18 15:51:45 +0100

    Fonction pour suppression reservation

---
+++ PHP/fonctions_suppr.php
@@ -0,0 +1,17 @@
+
+define('user','root', true );
+define('web', 'web', true );
+define('bdd','projet_bdd_iut', true );
+
+function suppr_reservation($id_reservation)
+    $link = mysqli_connect("localhost",'$user','$web', "$bdd");
+    if($link==NULL)die("pb connexion");
+    $requete="DELETE FROM RESERVATION WHERE id_reservation=$id_reservation ;";
+    //echo $requete;
+    if (!mysqli_query($link, $requete))
+    {
+        print("Message d'erreur :".mysqli_error($link));
+    }
+    mysqli_close($link);
+}
+
