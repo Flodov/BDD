acb037c52da9305f9a7bcc5b7ae591ede994fc37
Serobi <paul.nelaton@gmail.com>
2013-11-18 16:24:37 +0100

    Update Fonctions_affichages_formulaires_ajouts.php

--- PHP/Fonctions_affichages_formulaires_ajouts.php
+++ PHP/Fonctions_affichages_formulaires_ajouts.php
@@ -4,4 +4,31 @@ $user = root;
 $mdp = web;
 $bdd = projet_bdd_iut;
 
+
+function formulaire_ajout_Activite() {
+ ?>
+ <form method="get" action="page_ajout.php">
+  <div id="activite_champ1">
+  <label for="nom_acti">Nom de l'activité</label>
+  <input type="text" id="nom_acti" name="nom_act">
+  </div>
+  <div id="activite_champ2">
+		<label for="nom_lieu">Nom du lieu</label>
+		<select id="nom_lieu" name="nom_lieu">");
+		<?php
+    $connexion = mysqli_connect($host,$user,$mdp,$bdd);
+		$req = "SELECT * FROM Lieu";
+		$res = $connexion -> query($req);
+		while($ligne = $res -> Fetch_array()){
+		  print("<option value=\"".$ligne[0]."\">".$ligne[1]."</option>");
+		}
+		?>
+		</select></div><br/>
+	</div>
+	<input type="submit" value="valider">
+	</form>
+<?php 
+}
+
+
 ?>
