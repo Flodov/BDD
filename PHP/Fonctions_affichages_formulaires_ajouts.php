f54f5f8272459a8d9a02dbfe37aa03d6a5470016
Serobi <paul.nelaton@gmail.com>
2013-11-18 16:46:48 +0100

    Update Fonctions_affichages_formulaires_ajouts.php

--- PHP/Fonctions_affichages_formulaires_ajouts.php
+++ PHP/Fonctions_affichages_formulaires_ajouts.php
@@ -42,4 +42,28 @@ function formulaire_ajout_Lieu() {
 <?php
 }
 
+function formulaire_ajout_Tel() {
+ ?>
+ <form method="get" action="page_ajout.php">
+  <div id="tel_champ1">
+  <label for="num_tel">Numero de telephone</label>
+  <input type="text" id="num_tel" name="num_tel">
+  </div>
+  <div id="tel_champ2">
+		<label for="nom_personne">Nom du lieu</label>
+		<select id="nom_personne" name="nom_personne">");
+		<?php
+    		$connexion = mysqli_connect($host,$user,$mdp,$bdd);
+		$req = "SELECT id_personne, nom_personne FROM Personne";
+		$res = $connexion -> query($req);
+		while($ligne = $res -> Fetch_array()){
+		  print("<option value=\"".$ligne[0]."\">".$ligne[1]."</option>");
+		}
+		?>
+	</select></div><br/>
+	</div>
+	<input type="submit" value="valider">
+	</form>
+<?php
+}
 ?>
