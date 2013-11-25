d7c10ccf8be1bbb28210b95ee4171d5100ac2598
GHCrystal <pa.bonnel@laposte.net>
2013-11-25 16:14:08 +0100

    Update menu.php

--- menu/menu.php
+++ menu/menu.php
@@ -1,7 +1,7 @@
 <?php
 require_once("fonctions_fichier_html.php");
-require_once("Fonction_Affichage_modif_suppression.php");
-require_once("Fonctions_affichages_formulaires_ajouts.php");
+require_once("fonction_Affichage_modif_suppression.php");
+require_once("fonctions_affichages_formulaires_ajouts.php");
 require_once("formulaire_fonctions_modif.php");
 
 html_entete_page("Camping");
