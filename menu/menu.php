225e2f0f12bf240fa0850a6835bc20ebbbc9786c
GHCrystal <pa.bonnel@laposte.net>
2013-11-25 15:54:29 +0100

    Create menu.php

---
+++ menu/menu.php
@@ -0,0 +1,12 @@
+<?php
+require_once("fonctions_fichier_html.php");
+require_once("Fonction_Affichage_modif_suppression.php);
+require_once("Fonctions_affichages_formulaires_ajouts.php");
+require_once("formulaire_fonctions_modif.php");
+
+html_entete_page("Camping");
+
+affichage_menu();
+
+html_fin_page();
+?>
